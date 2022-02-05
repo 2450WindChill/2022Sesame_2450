7382459d5007d65ab28a67980036d4b6bc3025fb
cdhrobotics <98065672+cdhrobotics@users.noreply.github.com>
2022-02-05 10:02:55 -0600

    commented print statement

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -68,6 +68,6 @@ public class Robot extends TimedRobot {
     // of the robot forward and backward, and the Y axis of the right stick
     // moves the right side of the robot forward and backward.
     m_robotDrive.tankDrive(m_driverController.getLeftY(), m_driverController.getRightY());
-    System.out.println("Value of B button: " + m_driverController.getBButton());
+    //System.out.println("Value of B button: " + m_driverController.getBButton());
   }
 }

44fbb4eee3baae61e9b8fafecf8154a9c6ebc3ef
ThomasRC-bit <cunningham.thomas.20@c-dh.org>
2022-06-16 19:21:50 -0500

    Changed motors to Victor SPX

--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
@@ -10,13 +13,13 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrainSubsystem extends SubsystemBase {
   // Defining motors and putting the right motors in a motor control group
-  public final CANSparkMax m_rightMotor = new CANSparkMax(1, MotorType.kBrushed);
-  public final CANSparkMax m_rightMotor2 = new CANSparkMax(2, MotorType.kBrushed);
+  public final WPI_VictorSPX m_rightMotor = new WPI_VictorSPX(1);
+  public final WPI_VictorSPX m_rightMotor2 = new WPI_VictorSPX(2);
   public final MotorControllerGroup rightMotorGroup = new MotorControllerGroup(m_rightMotor, m_rightMotor2);
 
   // Defining motors and putting the left motors in a motor control group
-  public final CANSparkMax m_leftMotor = new CANSparkMax(3, MotorType.kBrushed);
-  public final CANSparkMax m_leftMotor2 = new CANSparkMax(4, MotorType.kBrushed);
+  public final WPI_VictorSPX m_leftMotor = new WPI_VictorSPX(3);
+  public final WPI_VictorSPX m_leftMotor2 = new WPI_VictorSPX(4);
   public final MotorControllerGroup leftMotorGroup = new MotorControllerGroup(m_leftMotor, m_leftMotor2);
 
   // Takes motor control groups and puts them in a differential drive
@@ -28,10 +31,10 @@ public class DriveTrainSubsystem extends SubsystemBase {
     double rate = 0.4; //drive rate
 
     // Inital acceleration of the robot (adjuste)
-    m_rightMotor.setOpenLoopRampRate(rate);
-    m_rightMotor2.setOpenLoopRampRate(rate);
-    m_leftMotor.setOpenLoopRampRate(rate);
-    m_leftMotor2.setOpenLoopRampRate(rate);
+    m_rightMotor.configOpenloopRamp(rate);
+    m_rightMotor2.configOpenloopRamp(rate);
+    m_leftMotor.configOpenloopRamp(rate);
+    m_leftMotor2.configOpenloopRamp(rate);
 
     // Inverting the motors (as needed)
   
@@ -42,7 +45,7 @@ public class DriveTrainSubsystem extends SubsystemBase {
   }
 
   public void takeXboxInputs(XboxController xbox) {
-    m_robotDrive.curvatureDrive((-xbox.getLeftTriggerAxis() + xbox.getRightTriggerAxis()), xbox.getLeftX() * .75 //turn rate
+    m_robotDrive.curvatureDrive((xbox.getLeftTriggerAxis() + -xbox.getRightTriggerAxis()), xbox.getLeftX() * .75 //turn rate
     , xbox.getAButton());
     //m_robotDrive.curvatureDrive(0, 0, xbox.getAButton());
   }
